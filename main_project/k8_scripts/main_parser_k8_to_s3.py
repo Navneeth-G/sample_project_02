43c8a0d35db8654ecea9a44f33f770f32a3ee95b
Navneeth-G <navneeth.g99@gmail.com>
2025-06-13 16:42:02 +0530

    Update main_parser_k8_to_s3.py

--- main_project/k8_scripts/main_parser_k8_to_s3.py
+++ main_project/k8_scripts/main_parser_k8_to_s3.py
@@ -1,1 +1,13 @@
+def k8_to_aws_s3_main(config_dict):
+  farm_list = config_dict["farm_list"]
+  file_path_template = config_dict["template_path"]
+  s3_bucket = config_dict["s3_bucket_name"]
+  index_name = config_dict["index_name"]
+  s3_key_prefix = config_dict["s3_key"]
+  aws_access_key = config_dict["aws_access_key"]
+  aws_secret_key = config_dict["aws_secret_key"]
+  timezone = config_dict["es_timezone"]
+  temp_output_dir = config_dict["temp_output_dir"]
+  
+
 
